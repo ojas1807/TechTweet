2ceee2419cbe1d23f5e1f9caeef9f28c53b02f58
Shrey Nagda <shreynagda2714@gmail.com>
2024-09-20 12:29:28 +0530

    Login and register route created

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import { userRef } from "../utils/strings.js";
+import { postRef, userRef } from "../utils/strings.js";
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: Number, required: true, unique: true },
   password: { type: String, required: true },
-  profile_pic: { type: String, required: true, default: "" },
+  profile_pic: { type: String, default: "" },
   about: { type: String },
-  posts: [{ type: mongoose.Schema.ObjectId }],
+  posts: [{ type: mongoose.Schema.ObjectId, ref: postRef }],
 });
 
 const User = mongoose.model(userRef, userSchema);
