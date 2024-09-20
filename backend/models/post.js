6d9f0f01b0aced8f1b3d34f9066ba6019f3a829d
Shrey Nagda <shreynagda2714@gmail.com>
2024-09-20 12:31:43 +0530

    added userid to post schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,11 +1,13 @@
 import mongoose, { mongo } from "mongoose";
-import { postRef } from "../utils/strings.js";
+import { postRef, userRef } from "../utils/strings.js";
 
 const postSchema = mongoose.Schema({
   photos: [{ type: String }],
   caption: { type: String },
   type: { type: String, default: "post" },
   tags: [{ type: String }],
+  likes: { type: Number, default: 0 },
+  user_id: { type: mongoose.Schema.ObjectId, ref: userRef },
 });
 
 const Post = mongoose.model(postRef, postSchema);
