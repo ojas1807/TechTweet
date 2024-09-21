0ea9844f1bc65a04c0a3ef3c1e6846acf4b68b27
chinmayeeks <chinmayeekedar@gmail.com>
2024-09-21 09:40:54 +0530

    yasssss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,6 @@ import AlumniStories from "./comps/aluministori"; // Ensure the correct path
 import AboutUs from "./comps/aboutUs";
 import LoginForm from "./auth/login.jsx";
 import Signup from "./auth/signup.jsx";
-import '@fontsource/josefin-sans';  // If you're using a bundler like Webpack
 
 
 function App() {
